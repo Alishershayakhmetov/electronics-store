5073b52e4f132909eee2f197ed09223b613efeea
alishershayakhmetov <shayakhmetovalisher1@gmail.com>
2024-01-09 22:45:18 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MainPage from './mainpage'
+import Header from './components/header.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MainPage />
+  </React.StrictMode>
+)
+
