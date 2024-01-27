14a16cfaef9f4c72f6294e1e2d6c403cb060f526
alishershayakhmetov <shayakhmetovalisher1@gmail.com>
2024-01-27 15:51:02 +0600

    added a product page

---
+++ src/components/carousel.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const ImageCarousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="carousel-container">
+      <ul className="carousel">
+        {images.map((image, index) => (
+          <li key={index} className={index === currentIndex ? 'slide active' : 'slide'}>
+            <img src={"src/assets/notebook/" + image.src} alt={image.alt} style={{"width":"500px","height":"300px"}} />
+          </li>
+        ))}
+      </ul>
+      <button onClick={prevSlide}>Prev</button>
+      <button onClick={nextSlide}>Next</button>
+    </div>
+  );
+};
+
+export default ImageCarousel;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainPage from './mainpage'
-import Header from './components/header.jsx'
+import Product from './product'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainPage />
+    <Product />
   </React.StrictMode>
 )
 
